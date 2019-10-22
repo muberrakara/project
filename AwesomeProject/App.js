82f014f4ccbd434fe9a9d558c99ee10a354bf0f7
Muberra <muberraakara@gmail.com>
2019-10-22 13:44:53 +0300

    First commit

---
+++ AwesomeProject/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import CalculatorScreen from './screens/CalculatorScreen';
+
+export default class App extends React.Component {
+  render(){
+    return <CalculatorScreen />;
+  }
+}
+
+
